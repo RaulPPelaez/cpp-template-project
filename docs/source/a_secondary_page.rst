4623b03a0ddd21fd490c3bb4d46d25f8f8fc9e9f
RaulPPealez <raulppelaez@gmail.com>
2024-09-11 18:19:45 +0200

    add docs, update some tests

---
+++ docs/source/a_secondary_page.rst
@@ -0,0 +1,9 @@
+A secondary page
+================
+
+We can also summon the documentation for a particular function here
+
+.. doxygenfunction:: my_library::add
+
+
+I can include internal links to functions like :cpp:func:`my_library::very_cool_function`
--- src/my_library.h
+++ src/my_library.h
@@ -6,8 +6,21 @@
 
 namespace my_library{
 
+  /**
+   * @brief A very cool function that prints some messages
+   *
+   * This function is very cool and prints some messages to the console.
+   * It is also documented using Doxygen style comments.
+   */
   void very_cool_function();
 
+  /**
+   * @brief Adds two numbers
+   *
+   * @param a The first number
+   * @param b The second number
+   * @return The sum of the two numbers
+   */
   double add(double a, double b);
 
 }
